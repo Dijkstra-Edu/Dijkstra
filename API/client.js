3513163b5779052011e96b87c9469a0454c76e4d
JRS296 <jonathansamuel296@gmail.com>
2023-09-12 20:18:46 +0530

    v3.1.1

--- API/client.js
+++ API/client.js
@@ -2,9 +2,9 @@
 import axios from "axios";
 
 //Main
-//const client = axios.create({baseURL: 'https://blog-app-service-gag1.onrender.com/api'});
+const client = axios.create({baseURL: 'https://blog-app-service-gag1.onrender.com/api'});
 
 //Dev
-const client = axios.create({baseURL: 'http://20.204.202.169:3000/api'});
+//const client = axios.create({baseURL: 'http://20.204.202.169:3000/api'});
 
 export default client;
--- components/Screens/Home.js
+++ components/Screens/Home.js
@@ -1,5 +1,5 @@
-import { useState, useEffect, useCallback, useMemo } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { useState, useEffect, useCallback, useMemo, useRef } from 'react';
+import { ActivityIndicator, Animated, StyleSheet, FlatList, View, Text } from 'react-native';
 
 import FocusAwareStatusBar from '../FocusAwareStatusBar'
 import PostListItems from '../PostListItems';
@@ -17,21 +17,24 @@ export default function Home({ navigation }) {
   const [latestPosts, setLatestPosts] = useState([]);
   const [reachedToEnd, setReachedToEnd] = useState(false);
   const [busy, setBusy] = useState(false);
+  const [loading1, setloading1] = useState(true);
+  const [loading2, setloading2] = useState(true);
+
 
   const fetchFeaturedPost = async () => {
     const { error, posts } = await getFeaturedPost();
     if (error) return console.log(error);
-
     //console.log(posts);
     setFeaturedPosts(posts);
+    setloading1(false);
   }
 
   const fetchLatestPost = async () => {
     const { error, posts } = await getLatestPosts(limit, pageNo);
     if (error) return console.log(error);
-
     //console.log(posts);
     setLatestPosts(posts);
+    setloading2(false);
   }
 
   const fetchMorePosts = async () => {
@@ -88,34 +91,76 @@ export default function Home({ navigation }) {
 
   // return (<Slider onSlidePress={fetchSinglePost} data={featuerdPosts} title="Featured Posts" />);
 
-  return <View>
-    <FocusAwareStatusBar backgroundColor="rgba(255,255,255,1)" barStyle="dark-content"/>
-    <FlatList
-      removeClippedSubviews
-      data={latestPosts}
-      keyExtractor={(item) => item.id}
-      contentContainerStyle={{ paddingHorizontal: 10, paddingBottom: 20 }}
-      ListHeaderComponent={Carousel}
-      ItemSeparatorComponent={itemSeparatorComponent}
-      renderItem={memoizedValue}
-      onEndReached={fetchMorePosts}
-      onEndReachedThreshold={0}
-      ListFooterComponent={() => {
-        return reachedToEnd ? (
-          <View>
-            <Text style={{
-              fontWeight: "bold",
-              color: "#383838",
-              textAlign: "center",
-              paddingVertical: 15,
-            }}>
-              You Reached the End!
-            </Text>
-            {/* <Icon name="rocket" size={30} color="#900" /> */}
-          </View>
-        ) : null;
-      }}
-    />
-  </View>
+  const FadeInView = props => { //Fix this by next update
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+    }, [fadeAnim]);
+
+    return (
+      <Animated.View // Special animatable View
+        style={{
+          ...props.style,
+          opacity: fadeAnim, // Bind opacity to animated value
+        }}>
+        {props.children}
+      </Animated.View>
+    );
+  };
+
+  if (loading1 && loading2) {
+    return <View style={[styles.container, styles.horizontal]}>
+      <ActivityIndicator size="large" color="#008000" />
+    </View>
+    // return <Loader />
+  } else {
+    //return <FadeInView>
+      return <View>
+        <FocusAwareStatusBar backgroundColor="rgba(255,255,255,1)" barStyle="dark-content" />
+        <FlatList
+          removeClippedSubviews
+          data={latestPosts}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={{ paddingHorizontal: 10, paddingBottom: 20 }}
+          ListHeaderComponent={Carousel}
+          ItemSeparatorComponent={itemSeparatorComponent}
+          renderItem={memoizedValue}
+          onEndReached={fetchMorePosts}
+          onEndReachedThreshold={0}
+          ListFooterComponent={() => {
+            return reachedToEnd ? (
+              <View>
+                <Text style={{
+                  fontWeight: "bold",
+                  color: "#383838",
+                  textAlign: "center",
+                  paddingVertical: 15,
+                }}>
+                  You Reached the End!
+                </Text>
+                {/* <Icon name="rocket" size={30} color="#900" /> */}
+              </View>
+            ) : null;
+          }}
+        />
+      </View>
+    //</FadeInView>
+  }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+});
